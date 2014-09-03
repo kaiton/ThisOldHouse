830bfe6b9cc7a94c20880448141ed7474ef34c96
Kaiton Williams <kaiton@cs.cornell.edu>
2014-09-02 19:21:07 -0500

    instrumenting methods

--- app/src/main/java/com/wikispaces/cmip_training_camp/thisoldhouse/MainActivity.java
+++ app/src/main/java/com/wikispaces/cmip_training_camp/thisoldhouse/MainActivity.java
@@ -6,13 +6,53 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate run");
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() method");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG, "onRestart() method");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() method");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() method");
+    }
+
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop() method");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() method");
     }
 
 
